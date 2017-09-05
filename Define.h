f1a103984802c35fc5decbfb9389b5d116922d0f
HenryYang <30862438+HenryYang99@users.noreply.github.com>
2017-09-05 18:17:15 +0800

    Add files via upload

---
+++ Define.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define  ID          "161220155"        // your id
+#define  PASSWORD    "123456"           // your password
+#define  SERVER_IP   "114.212.134.119"	    // server ip
+#define  SERVER_PORT 10087				// server port
